542fce87f59c3181251c2f999b244cdceadfd073
Alexander Hoth <alexanderhoth@pressmatrix.de>
2025-11-07 02:51:06 +0100

    Pinning tailwind elements

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
+pin "@tailwindplus/elements", to: "@tailwindplus--elements.js" # @1.0.18
